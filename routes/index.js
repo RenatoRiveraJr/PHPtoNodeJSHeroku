30920aacc3e89d0cc14491f3283bb27b6d807885
Renato Rivera Jr <renato.aspuria.rivera@gmail.com>
2017-11-08 16:34:35 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,20 @@ router.get('/', function(req, res, next) {
 });
 router.post('/', function(req, res){
     //res.writeHead(200, {'Content-Type': 'text/html'});
-    res.send("Hey! " + req.param('username'));
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://heroku_1jbzqctc:&7c4fPwVBcUDTcu1@ds251245.mlab.com:51245/heroku_1jbzqctc";
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var query = { name: req.param('username') };
+        db.collection("ORDERS").find(query).toArray(function(err, result) {
+            if (err) throw err;
+            res.send("Hey! " + req.param('username') + "Here is your order: " +
+            result);
+            db.close();
+        });
+    });
+
 });
 
 module.exports = router;
