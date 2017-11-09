66d6c4c35cc51988e5682c57219c5028a0b26a14
RenatoRiveraJr <renato.aspuria.rivera@gmail.com>
2017-11-08 17:54:34 -0800

    Fixed mongoDB Functionatlity

--- routes/index.js
+++ routes/index.js
@@ -3,98 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
-router.post('/', function(req, res){
-    //res.writeHead(200, {'Content-Type': 'text/html'});
-    var mongodb = require('mongodb');
+   //res.render('index', { title: 'Express'});
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://r39rivera:Holleriv12@ds251245.mlab.com:51245/heroku_1jbzqctc";
 
-// Create seed data
 
-    var seedData = [
-        {
-            decade: '1970s',
-            artist: 'Debby Boone',
-            song: 'You Light Up My Life',
-            weeksAtOne: 10
-        },
-        {
-            decade: '1980s',
-            artist: 'Olivia Newton-John',
-            song: 'Physical',
-            weeksAtOne: 10
-        },
-        {
-            decade: '1990s',
-            artist: 'Mariah Carey',
-            song: 'One Sweet Day',
-            weeksAtOne: 16
-        }
-    ];
-
-// Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
-
-    var uri = 'mongodb://heroku_1jbzqctc:Holleriv12@ds251245.mlab.com:51245/heroku_1jbzqctc';
-
-    mongodb.MongoClient.connect(uri, function(err, db) {
-
-        if(err) throw err;
-
-        /*
-         * First we'll add a few songs. Nothing is required to create the
-         * songs collection; it is created automatically when we insert.
-         */
-
-        var songs = db.collection('songs');
-
-        // Note that the insert method can take either an array or a dict.
-
-        songs.insert(seedData, function(err, result) {
-
-            if(err) throw err;
-
-            /*
-             * Then we need to give Boyz II Men credit for their contribution
-             * to the hit "One Sweet Day".
-             */
-
-            songs.update(
-                { song: 'One Sweet Day' },
-                { $set: { artist: 'Mariah Carey ft. Boyz II Men' } },
-                function (err, result) {
-
-                    if(err) throw err;
-
-                    /*
-                     * Finally we run a query which returns all the hits that spend 10 or
-                     * more weeks at number 1.
-                     */
-
-                    songs.find({ weeksAtOne : { $gte: 10 } }).sort({ decade: 1 }).toArray(function (err, docs) {
-
-                        if(err) throw err;
-
-                        docs.forEach(function (doc) {
-                            console.log(
-                                'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] +
-                                ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.'
-                            );
-                        });
-
-                        // Since this is an example, we'll clean up after ourselves.
-                        //songs.drop(function (err) {
-                          //  if(err) throw err;
-
-                            // Only close the connection when your app is terminating.
-                            db.close(function (err) {
-                                if(err) throw err;
-                            });
-                        });
-                    });
-                }
-            );
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        db.collection("ORDERS").find({name: "Renato"}).toArray(function (err, docs) {
+            if (err) throw err;
+            docs.forEach(function (doc) {
+                res.send(doc['name'] + " " + doc['order']);
+            });
         });
     });
+});
+router.post('/', function(req, res){
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://r39rivera:Holleriv12@ds251245.mlab.com:51245/heroku_1jbzqctc";
+
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        db.collection("ORDERS").find({name: JSON.stringify(req.param('username'))}).toArray(function (err, docs) {
+            if (err) throw err;
+            docs.forEach(function (doc) {
+                res.send(doc['name'] + " " + doc['order']);
+            });
+        });
+    });
+});
 
 
 module.exports = router;
