b5888a294f900bc49e62d9ffb71318f5094739a5
RenatoRiveraJr <renato.aspuria.rivera@gmail.com>
2017-11-14 13:07:00 -0800

    edited databasee.js

--- controllers/database.js
+++ controllers/database.js
@@ -11,7 +11,7 @@ module.exports.getAllOrders =  function (request, response) {
         db.collection("ORDERS").find().toArray(function (err, docs) {
             if(err) throw err;
 
-            response.render('getAllRoutes', {results: docs});
+            response.render('getAllOrders', {results: docs});
 
         });
     });
